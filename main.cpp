3a942354a0f26eab92bd8a6a3adf3ea0ecd35221
Evan <comtesseevan@gmail.com>
2025-07-09 00:09:50 +0200

    feat: added and linked GLAD

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <glfw3.h>
+// #include <glad/glad.h> Already included with OpenGL
 
 int main(){
-    std::cout << "Hello World, Evan, I'm using GLFW Now !\n";
+    std::cout << "Hello World, Evan, I'm using GLFW Now, and GLAD !\n";
 
     GLFWwindow* window;
 
