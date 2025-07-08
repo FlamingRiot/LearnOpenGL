6d81046b687ae4db483c80b1cf1efdcb40f8b9d7
Evan <comtesseevan@gmail.com>
2025-07-09 00:29:11 +0200

    fix: GLFW not correctly linked to project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <glfw3.h>
 // #include <glad/glad.h> Already included with OpenGL
 
 int main(){
     std::cout << "Hello World, Evan, I'm using GLFW Now, and GLAD !\n";
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
 
